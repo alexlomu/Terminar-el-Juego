0e836198503675cf67736d65e56a186dda3faa61
alexlomu <91721507+alexlomu@users.noreply.github.com>
2021-11-13 20:09:51 +0100

    intento de modo de juego ia

--- terminar_juego.py
+++ terminar_juego.py
@@ -22,7 +22,21 @@ def dificultad():
             print("El número esta entre 0 y 1000000000000, tienes un total de 100 intentos")
     else:
         print("Selecciona una dificultad válida")   
-          
+
+def modo():
+    print("Modo de juego 1: Juega usted")
+    print("Modo de juego2: Juega la IA")
+    modo = int(input("Introduce al modo que quieras jugar(1-2)"))
+    global modo_escogido
+    modo_escogido = modo
+    if modo_escogido == 1:
+        print("Has escogido el modo de juego 1")
+    if modo_escogido == 2:
+        print("Has escogido el modo de juego 2")
+    if 2 > modo_escogido < 1:
+        print("Escoge un modo de juego válido(1-2)")
+    return modo_escogido
+
             
 def comparar_numeros():
     jugadas = 0
@@ -44,6 +58,26 @@ def comparar_numeros():
         print("Felicidades has ganado, el número era", str(numero))
         print("En total has hecho:", str(jugadas), "intentos.")
 
+def comparar_numeros_ia(num_min, num_max, jugadas_max):
+    jugadas = 0
+    while jugadas <= jugadas_max:
+        x = num_max - num_min
+        numero_ia = int(x/2)
+        print("La IA ha probado con", str(numero_ia))
+        jugadas += 1
+        if numero_ia < numero:
+            print("El número probado es demasiado pequeño, la IA lleva", str(jugadas), "intentos")
+            num_min = numero_ia
+        elif numero_ia > numero:
+            print("El número probado es demasiado grande, la IA lleva", str(jugadas), "intentos")
+            num_max = numero_ia
+        elif numero_ia == numero:
+            break
+    if jugadas == jugadas_max:
+        print("Vaya, se le han acabado los intentos a la IA")
+    if numero_ia == numero:
+        print("La IA ha adivinado el número!, ha usado", str(jugadas), "intentos")
+
 
 dificultad()    
 if nivel == 1:
@@ -51,24 +85,44 @@ if nivel == 1:
     jugadas_max = 10
     num_min = 0
     num_max = 100
-    comparar_numeros()
+    modo()
+    modo = modo_escogido
+    if modo == 1:
+        comparar_numeros()
+    if modo == 2:
+        comparar_numeros_ia(0,100, 10)
 elif nivel == 2:
     numero = random.randint(0,1000)
     jugadas_max = 20
     num_min = 0
     num_max = 1000
-    comparar_numeros()
+    modo()
+    modo = modo_escogido
+    if modo == 1:
+        comparar_numeros()
+    if modo == 2:
+        comparar_numeros_ia(0,1000, 20)
 elif nivel == 3:
     numero = random.randint(0,1000000)
     jugadas = 0
     jugadas_max = 50
     num_min = 0
     num_max = 1000000
-    comparar_numeros()    
+    modo()
+    modo = modo_escogido
+    if modo == 1:
+        comparar_numeros()
+    if modo == 2:
+        comparar_numeros_ia(0,1000000, 50)    
 elif nivel == 4:
     numero = random.randint(0,1000000000000)
     jugadas = 0
     jugadas_max = 100
     num_min = 0
     num_max = 1000000000000
-    comparar_numeros() 
+    modo()
+    modo = modo_escogido
+    if modo == 1:
+        comparar_numeros()
+    if modo == 2:
+        comparar_numeros_ia(0,1000000000000, 100) 
